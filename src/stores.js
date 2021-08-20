02802d97ec5e97e63f03a586fe5b90f212ddc0d4
Sam Thomas <>
2021-08-20 18:55:29 +0100

    Reverted stores.js changes because they seemed to have caused the initial error

--- src/stores.js
+++ src/stores.js
@@ -1,15 +1,6 @@
 import { writable } from "svelte/store";
-let stored = localStorage.getItem("todos");
-let todosJSON;
-if (stored) {
-  try {
-    todosJSON = JSON.parse(stored);
-  } catch (error) {
-    console.error(`Error parsing todos JSON: "${error}". JSON: "${stored}"`);
-    todosJSON = [];
-  }
-} else {
-  todosJSON = [
+export let todos = writable(
+  JSON.parse(localStorage.getItem("todos")) || [
     {
       id: Math.floor(Math.random() * 100000000),
       completed: true,
@@ -35,9 +26,8 @@ if (stored) {
         "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis sint ratione facilis, natus cum labore eos quasi eum porro. Quam, est natus repellat ab ipsam ut accusamus tempore necessitatibus, perspiciatis, temporibus dicta iure! Necessitatibus, dignissimos nisi, accusantium corrupti illum culpa libero debitis ratione veritatis maiores quae sit ipsa iste consequuntur?",
       description: "Hello world",
     },
-  ];
-}
-export let todos = writable(todosJSON);
+  ]
+);
 export const unsubscribe = todos.subscribe((val) =>
   localStorage.setItem("todos", JSON.stringify(val))
 );
