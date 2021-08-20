afa78b5ce96ea68829d21c01dc71906e431935bc
Sam Thomas <>
2021-08-20 17:35:14 +0100

    Added todos JSON error catching.

--- src/stores.js
+++ src/stores.js
@@ -1,6 +1,14 @@
 import { writable } from "svelte/store";
-export let todos = writable(
-  JSON.parse(localStorage.getItem("todos")) || [
+let todosJSON;
+try {
+  todosJSON = JSON.parse(localStorage.getItem("todos"));
+} catch (error) {
+  console.error(
+    `Error parsing todos JSON: "${error}". JSON: "${localStorage.getItem(
+      "todos"
+    )}"`
+  );
+  todosJSON = [
     {
       id: Math.floor(Math.random() * 100000000),
       completed: true,
@@ -26,8 +34,9 @@ export let todos = writable(
         "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis sint ratione facilis, natus cum labore eos quasi eum porro. Quam, est natus repellat ab ipsam ut accusamus tempore necessitatibus, perspiciatis, temporibus dicta iure! Necessitatibus, dignissimos nisi, accusantium corrupti illum culpa libero debitis ratione veritatis maiores quae sit ipsa iste consequuntur?",
       description: "Hello world",
     },
-  ]
-);
+  ];
+}
+export let todos = writable(todosJSON);
 export const unsubscribe = todos.subscribe((val) =>
   localStorage.setItem("todos", JSON.stringify(val))
 );
